899efc7e9a7217af58650015246ab4303eecc94c
Gurpriksha Kalra <gurpriksha.kalra@gmail.com>
2021-03-10 02:12:24 +0530

    sketch & index

--- sketch.js
+++ sketch.js
@@ -9,9 +9,11 @@ var stand1,stand2;
 var ball;
 var slingShot;
 var polygon_img;
+
 function preload(){
   polygon_img=loadImage("polygon.png");
 }
+
 function setup() {
   createCanvas(900,400);
   engine = Engine.create();
@@ -30,16 +32,19 @@ function setup() {
   block5 = new Block(420,275,30,40);
   block6 = new Block(450,275,30,40);
   block7 = new Block(480,275,30,40);
+
   //level two
   block8 = new Block(330,235,30,40);
   block9 = new Block(360,235,30,40);
   block10 = new Block(390,235,30,40);
   block11 = new Block(420,235,30,40);
   block12 = new Block(450,235,30,40);
+
   //level three
   block13 = new Block(360,195,30,40);
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
+
   //top
   block16 = new Block(390,155,30,40);
 
@@ -50,10 +55,12 @@ function setup() {
   blocks3 = new Block(700,175,30,40);
   blocks4 = new Block(730,175,30,40);
   blocks5 = new Block(760,175,30,40);
+
   //level two
   blocks6 = new Block(670,135,30,40);
   blocks7 = new Block(700,135,30,40);
   blocks8 = new Block(730,135,30,40);
+
   //top
   blocks9 = new Block(700,95,30,40);
 
@@ -64,6 +71,7 @@ function setup() {
   slingShot = new Slingshot(this.ball,{x:100,y:200});
 
 }
+
 function draw() {
   background(56,44,44); 
  
@@ -74,8 +82,10 @@ function draw() {
   text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
 
   ground.display();
+
   stand1.display();
   stand2.display();
+
   strokeWeight(2);
   stroke(15);
   fill("skyblue");
@@ -86,39 +96,48 @@ function draw() {
   block5.display();
   block6.display();
   block7.display();
+
   fill("pink");
   block8.display();
   block9.display();
   block10.display();
   block11.display();
   block12.display();
+
   fill("turquoise");
   block13.display();
   block14.display();
   block15.display();
+
   fill("grey");
   block16.display();
+
   fill("skyblue");
   blocks1.display();
   blocks2.display();
   blocks3.display();
   blocks4.display();
   blocks5.display();
+
   fill("turquoise");
   blocks6.display();
   blocks7.display();
   blocks8.display();
+
   fill("pink")
   blocks9.display();
+
   fill("gold");
   imageMode(CENTER)
   image(polygon_img ,ball.position.x,ball.position.y,40,40);
 
   slingShot.display();
 }
+
 function mouseDragged(){
   Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
 }
+
 function mouseReleased(){
   slingShot.fly();
 }
